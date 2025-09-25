c81c635790a0b667e96d888881c548cfc345686c
Minh Dang <minh.dang@omega365.com>
2025-09-25 12:25:06 +0700

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -75,7 +75,7 @@ app.use('/api/stats', statRoutes);
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
+  app.get('/{*any}', (req, res) => {
     res.send(path.join(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
